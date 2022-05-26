1ce36883dc76370d11c91c6b3fe36511cbc8cd6d
furqonfauzi <55476955+furqonfauzi@users.noreply.github.com>
2022-05-26 12:35:19 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,377 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+     <title>Muhammad Furqon Fajar Fauzi</title>
+
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+     <meta name="description" content="">
+     <meta name="keywords" content="">
+     <meta name="author" content="">
+     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+     <link rel="stylesheet" href="css/bootstrap.min.css">
+     <link rel="stylesheet" href="css/font-awesome.min.css">
+     <link rel="stylesheet" href="css/aos.css">
+
+     <!-- MAIN CSS -->
+     <link rel="stylesheet" href="css/tooplate-gymso-style.css">
+<!--
+Tooplate 2119 Gymso Fitness
+https://www.tooplate.com/view/2119-gymso-fitness
+-->
+</head>
+<body data-spy="scroll" data-target="#navbarNav" data-offset="50">
+
+    <!-- MENU BAR -->
+    <nav class="navbar navbar-expand-lg fixed-top">
+        <div class="container">
+
+            <a class="navbar-brand" href="index.php">PORTOFOLIO </a>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-lg-auto">
+                    <li class="nav-item">
+                        <a href="#home" class="nav-link smoothScroll">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#feature" class="nav-link smoothScroll">Intro</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#about" class="nav-link smoothScroll">About </a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="#class" class="nav-link smoothScroll">Portofolio</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="#schedule" class="nav-link smoothScroll">Team</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="#contact" class="nav-link smoothScroll">Contact</a>
+                    </li>
+                </ul>
+
+                <ul class="social-icon ml-lg-3">
+                   <li><a href="https://github.com/furqonfauzi" class="fa fa-github"></a></li>
+                                            <li><a href="https://facebook.com/furqon_fauzi" class="fa fa-facebook"></a></li>
+                </ul>
+            </div>
+
+        </div>
+    </nav>
+    <
+
+     <!-- HERO -->
+     <section class="hero d-flex flex-column justify-content-center align-items-center" id="home">
+
+            <div class="bg-overlay"></div>
+
+               <div class="container">
+                    <div class="row">
+
+                         <div class="col-lg-8 col-md-10 mx-auto col-12">
+                              <div class="hero-text mt-5 text-center">
+
+                                    <h6 data-aos="fade-up" data-aos-delay="300">Hallo ,Selamat Datang di Website Official Saya <br>  <small>- Muhammad Furqon Fajar Fauzi -</small></h6>
+
+
+                                    <h1 class="text-white" data-aos="fade-up" data-aos-delay="500"> Progremer Web Dev <br>( Front-End and Back-End )</h1>
+
+                                    <a href="#feature" class="btn custom-btn mt-3" data-aos="fade-up" data-aos-delay="600">Get started</a>
+
+                                    <a href="#about" class="btn custom-btn bordered mt-3" data-aos="fade-up" data-aos-delay="700">learn more</a>
+                                   
+                              </div>
+                         </div>
+
+                    </div>
+               </div>
+     </section>
+
+
+    <section class="feature" id="feature">
+        <div class="container">
+            <div class="row">
+
+                <div class="d-flex flex-column justify-content-center ml-lg-auto mr-lg-5 col-lg-5 col-md-6 col-12">
+                    <h2 class="mb-3 text-white" data-aos="fade-up">Apa Itu Web Developer ?</h2>
+
+                    <h6 class="mb-4 text-white" data-aos="fade-up">Pengembang Web yang bekerja untuk merancang, membuat, dan memelihara situs web dan aplikasi web</h6>
+
+                    <p data-aos="fade-up" data-aos-delay="200">bisa diartikan pengembang web adalah seseorang yang berhubungan dengan pembuatan suatu website.</p>
+
+                   
+                </div>
+
+                <div class="mr-lg-auto mt-3 col-lg-4 col-md-6 col-12">
+                     <div class="about-working-hours">
+                          <div>
+
+                                <!-- <h2 class="mb-4 text-white" data-aos="fade-up" data-aos-delay="500">Jenis WEB DEV</h2>
+ -->
+                               <strong class="mt-3 d-block" data-aos="fade-up" data-aos-delay="700">Front End</strong>
+
+                                <p data-aos="fade-up" data-aos-delay="800">Membuat tampilan website yang dilihat oleh user serta memastikannya berfungsi.</p>
+
+                                <strong class="mt-3 d-block" data-aos="fade-up" data-aos-delay="700">Back End </strong>
+
+                                <p data-aos="fade-up" data-aos-delay="800">Membangun sebuah web dari sisi server yang berhubungan dengan logika, database, scripting, dan arsitektur dari sebuah website. </p>
+
+                                <strong class="mt-3 d-block" data-aos="fade-up" data-aos-delay="700">Full Stack </strong>
+
+                                <p data-aos="fade-up" data-aos-delay="800">Menjalankan peranan lintas fungsional, serta bertanggung jawab pada back-end dan front-end.</p>
+                               </div>
+                          </div>
+                     </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+
+     <!-- ABOUT -->
+     <section class="about section" id="about">
+               <div class="container">
+                    <div class="row">
+
+                            <div class="mt-lg-5 mb-lg-0 mb-4 col-lg-8 col-md-10 mx-auto col-12">
+                                <h2 class="mb-4" data-aos="fade-up" data-aos-delay="300">Hallo, Saya Adalah Web dev</h2>
+
+                                <p data-aos="fade-up" data-aos-delay="400">Saya adalah seorang fresh graduate dari jurusan Sarjana Informatika. Saya memiliki minat dibidang IT seperti pemrograman web dan jaringan. Saya dapat membangun website dengan framework codeigniter, MySQL, dan Javascript. Saya adalah seorang yang bertanggung jawab tinggi serta memiliki motivasi unutk mengembangkan diri dan suka mencoba hal-hal baru.</p>
+                                <p data-aos="fade-up" data-aos-delay="500"> Kemampuan yang dikuasai : <br> <br>PHP <br>MySQL <br>Codeigniter <br> Boostrap <br> API <br> jQuery </p>
+
+                            </div>
+
+                            <div class="ml-lg-auto col-lg-3 col-md-6 col-12" data-aos="fade-up" data-aos-delay="700">
+                                <div class="team-thumb">
+                                    <img src="images/team/team-image.jpg" class="img-fluid" alt="Trainer">
+
+                                    <div class="team-info d-flex flex-column">
+
+                                         <h3>Furqon Fauzi</h3>
+                                        <span>Web Developer</span>
+
+                                        <ul class="social-icon mt-3">
+                                            <li><a href="https://github.com/furqonfauzi" class="fa fa-github"></a></li>
+                                            <li><a href="https://facebook.com/furqon_fauzi" class="fa fa-facebook"></a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                    </div>
+               </div>
+     </section>
+
+
+
+     <!-- CLASS -->
+     <section class="class section" id="class">
+               <div class="container">
+                    <div class="row">
+
+                            <div class="col-lg-12 col-12 text-center mb-5">
+                                <h6 data-aos="fade-up">Wallcome To My Portofolio</h6>
+
+                                <h2 data-aos="fade-up" data-aos-delay="200">Website Yang Pernah Saya Buat</h2>
+                             </div>
+
+                            <div class="col-lg-4 col-md-6 col-12" data-aos="fade-up" data-aos-delay="400">
+                                <div class="class-thumb">
+                                    <img src="images/class/kasir.png" class="img-fluid" alt="Class">
+
+                                    <div class="class-info">
+                                        <h3 class="mb-1">Point Of Sales - Kusuma Batik</h3>
+
+                                        <span><strong>Create by</strong> - furqon fauzi</span>
+
+                                        <span class="class-price">A</span>
+
+                                        <p class="mt-3">Membuat siytem POS untuk mempermudah penjualan batik ( Tahun 2020 )</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="mt-5 mt-lg-0 mt-md-0 col-lg-4 col-md-6 col-12" data-aos="fade-up" data-aos-delay="500">
+                                <div class="class-thumb">
+                                    <img src="images/class/penjualan.png" class="img-fluid" alt="Class">
+
+                                    <div class="class-info">
+                                        <h3 class="mb-1">E-commerce - Samudra Ps II</h3>
+
+                                        <strong>Create by</strong> - furqon fauzi</span>
+
+                                        <span class="class-price">A+</span>
+
+                                        <p class="mt-3">Membuat sistem penjualan yang terintegradi dengan API raja ongkir, midtrans ( Tahun 2021 )</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="mt-5 mt-lg-0 col-lg-4 col-md-6 col-12" data-aos="fade-up" data-aos-delay="600">
+                                <div class="class-thumb">
+                                    <img src="images/class/perpus.jpg" class="img-fluid" alt="Class">
+
+                                    <div class="class-info">
+                                        <h3 class="mb-1">Sistem Infromasi Perpustakaan</h3>
+
+                                        <strong>Create by</strong> - furqon fauzi</span>
+
+                                        <span class="class-price">A</span>
+
+                                        <p class="mt-3">Membuat sitem yang dapat mencatat transaksi peminjaman buku ( Tahun 2021 )</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                    </div>
+               </div>
+     </section>
+
+<   
+     <!-- SCHEDULE -->
+     <section class="schedule section" id="schedule">
+               <div class="container">
+                    <div class="row">
+
+                            <div class="ml-lg-auto col-lg-3 col-md-6 col-12" data-aos="fade-up" data-aos-delay="700">
+                                <div class="team-thumb">
+                                    <img src="images/team/user8.jpg" class="img-fluid" alt="Trainer">
+
+                                    <div class="team-info d-flex flex-column">
+
+                                        <h3>Albert</h3>
+                                        <span>Manager</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="ml-lg-auto col-lg-3 col-md-6 col-12" data-aos="fade-up" data-aos-delay="700">
+                                <div class="team-thumb">
+                                    <img src="images/team/user6.jpg" class="img-fluid" alt="Trainer">
+
+                                    <div class="team-info d-flex flex-column">
+
+                                        <h3>Yohanes</h3>
+                                        <span>Enginer</span>
+                                     </div>
+                                </div>
+                            </div><div class="ml-lg-auto col-lg-3 col-md-6 col-12" data-aos="fade-up" data-aos-delay="700">
+                                <div class="team-thumb">
+                                    <img src="images/team/team-image.jpg" class="img-fluid" alt="Trainer">
+
+                                    <div class="team-info d-flex flex-column">
+
+                                        <h3>Furqon Fauzi</h3>
+                                        <span>Web Developer</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="mr-lg-auto mt-5 mt-lg-0 mt-md-0 col-lg-3 col-md-6 col-12" data-aos="fade-up" data-aos-delay="800">
+                                <div class="team-thumb">
+                                    <img src="images/team/user1.jpg" class="img-fluid" alt="Trainer">
+
+                                    <div class="team-info d-flex flex-column">
+
+                                        <h3>Catherina</h3>
+                                        <span>Team Manager</span>
+                                    </div>
+                                </div>
+                            </div>
+
+                    </div>
+               </div>
+     </section>
+
+
+     <!-- CONTACT -->
+     <section class="contact section" id="contact">
+          <div class="container">
+               <div class="row">
+
+                    <div class="ml-auto col-lg-5 col-md-6 col-12">
+                        <h2 class="mb-4 pb-2" data-aos="fade-up" data-aos-delay="200">Feel free to ask anything</h2>
+
+                        <form action="#" method="post" class="contact-form webform" data-aos="fade-up" data-aos-delay="400" role="form">
+                            <input type="text" class="form-control" name="cf-name" placeholder="Name">
+
+                            <input type="email" class="form-control" name="cf-email" placeholder="Email">
+
+                            <textarea class="form-control" rows="5" name="cf-message" placeholder="Message"></textarea>
+
+                            <button type="submit" class="form-control" id="submit-button" name="submit">Send Message</button>
+                        </form>
+                    </div>
+
+                    <div class="mx-auto mt-4 mt-lg-0 mt-md-0 col-lg-5 col-md-6 col-12">
+                        <h2 class="mb-4" data-aos="fade-up" data-aos-delay="600">Where you can <span>find us</span></h2>
+
+                        <p data-aos="fade-up" data-aos-delay="800"><i class="fa fa-map-marker mr-1"></i> Sluwok Lor, Tawangsari, Kec. Pengasih, Kabupaten Kulon Progo, Daerah Istimewa Yogyakarta</p>
+
+                        <div class="google-map" data-aos="fade-up" data-aos-delay="900">
+                          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d717.4846182173625!2d110.10633171018152!3d-7.889378666980705!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x987d09e529c6ca76!2zN8KwNTMnMjAuMCJTIDExMMKwMDYnMjMuNCJF!5e1!3m2!1sid!2sid!4v1653540754843!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        </div>
+                    </div>
+                    
+               </div>
+          </div>
+     </section>
+
+
+     <!-- FOOTER -->
+     <footer class="site-footer">
+          <div class="container">
+               <div class="row">
+
+                    <div class="ml-auto col-lg-4 col-md-5">
+                        <p class="copyright-text">Copyright &copy; 2022 Muhammad Furqon Fajar Fauzi.
+                        
+                    </div>
+
+                    <div class="d-flex justify-content-center mx-auto col-lg-5 col-md-7 col-12">
+                        <p class="mr-4">
+                            <i class="fa fa-envelope-o mr-1"></i>
+                            <a href="#">furqonfauzi28@gmail.com</a>
+                        </p>
+
+                        <p><i class="fa fa-phone mr-1"></i> 081 393 474 459</p>
+                    </div>
+                    
+               </div>
+          </div>
+     </footer>
+
+    <!-- Modal -->
+    <div class="modal fade" id="membershipForm" tabindex="-1" role="dialog" aria-labelledby="membershipFormLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+
+        <div class="modal-content">
+          <div class="modal-header">
+
+            <h2 class="modal-title" id="membershipFormLabel">Membership Form</h2>
+
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+     <!-- SCRIPTS -->
+     <script src="js/jquery.min.js"></script>
+     <script src="js/bootstrap.min.js"></script>
+     <script src="js/aos.js"></script>
+     <script src="js/smoothscroll.js"></script>
+     <script src="js/custom.js"></script>
+
+</body>
+</html>
